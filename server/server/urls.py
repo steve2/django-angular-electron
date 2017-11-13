a9ee1940188113498940a96b493c6247727fd12b
Steve <b0rg3r@gmail.com>
2017-11-13 15:38:03 -0600

    Initial Django project files.

---
+++ server/server/urls.py
@@ -0,0 +1,28 @@
+"""server URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import include
+from two_factor.urls import urlpatterns as tf_urls
+# from two_factor.gateways.twilio.urls import urlpatterns as tf_twilio_urls
+from two_factor.admin import AdminSiteOTPRequired
+
+admin.site.__class__ = AdminSiteOTPRequired
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include(tf_urls, 'two_factor'))
+]
