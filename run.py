92c4eb5f151d9a1f4f1d8790524c550fcb39c589
Steve <b0rg3r@gmail.com>
2017-11-13 19:41:15 -0600

    Appeasing the PEP8 standard.

--- run.py
+++ run.py
@@ -7,19 +7,24 @@ import os
 
 DEBUG_PORT = 8080
 
-def RunServer():
-    os.system('server\\env\\Scripts\\python.exe server\\manage.py runserver %s' % DEBUG_PORT)
 
-def RunElectron():
+def _run_server():
+    os.system(
+        'server\\env\\Scripts\\python.exe server\\manage.py runserver %s' % DEBUG_PORT)
+
+
+def _run_electron():
     os.system('client\\node_modules\\electron-prebuilt\\dist\\electron.exe client')
 
-def Main():
-    serverThread = Thread(target=RunServer)
-    serverThread.start()
-    electronThread = Thread(target=RunElectron)
-    electronThread.start()
-    serverThread.join()
-    electronThread.join()
+
+def _main():
+    server_thread = Thread(target=_run_server)
+    server_thread.start()
+    electron_thread = Thread(target=_run_electron)
+    electron_thread.start()
+    server_thread.join()
+    electron_thread.join()
+
 
 if __name__ == '__main__':
-    Main()
+    _main()
--- server/server/views.py
+++ server/server/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views.generic import TemplateView
+
 
 class IndexTemplate(TemplateView):
     """
     The base template for our AngularJS application client.
-
     """
     template_name = 'index.html'
