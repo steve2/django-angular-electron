6514eefab34a838f81935236e6122bc50535bb53
Steve <b0rg3r@gmail.com>
2017-11-13 19:21:00 -0600

    Fixed build.py when being run from other directories.

--- client/build.py
+++ client/build.py
@@ -1,3 +1,5 @@
 import os
 
-os.system('lessc static/styles/site.less static/styles/site.css')
+cwd = os.path.dirname(os.path.realpath(__file__))
+
+os.system('lessc %s/static/styles/site.less %s/static/styles/site.css' % (cwd, cwd))
