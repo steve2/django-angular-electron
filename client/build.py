b7e033ef369478f8ff66d17aafaf0cd6d6edf9fa
Steve <b0rg3r@gmail.com>
2017-11-15 05:01:33 -0600

    client/build.py now prints the command and output.

--- client/build.py
+++ client/build.py
@@ -2,4 +2,12 @@ import os
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 
-os.system('lessc %s/static/styles/site.less %s/static/styles/site.css' % (cwd, cwd))
+
+def build_client(): # Build client assets.
+  path = 'lessc %s/static/styles/site.less %s/static/styles/site.css'
+  out = os.system(path % (cwd, cwd))  
+  print((path % (cwd, cwd)) + ' [' + str(out) + ']')
+
+
+if __name__ == '__main__':
+  build_client()
