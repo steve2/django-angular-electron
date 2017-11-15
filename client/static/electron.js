49e76b81ef39c699e23aa1682f0d7b7831934822
Steve <b0rg3r@gmail.com>
2017-11-15 00:08:30 -0600

    Added electron.js for Electron utility functions.

---
+++ client/static/electron.js
@@ -0,0 +1,28 @@
+"use strict";
+
+if (typeof require === 'function')
+    var remote = require('electron').remote;
+
+function registerCloseControls(className) {
+    // Registers the Window close control events.
+    if (remote) {
+        let elements = document.getElementsByClassName(className);
+        for (let i = 0; i < elements.length; i++)
+            elements[i].addEventListener('click', function (event) {
+                var window = remote.getCurrentWindow();
+                window.close();
+            });
+    }
+}
+
+function isElectron() {
+    if (typeof require !== 'function') return false;
+    if (typeof window !== 'object') return false;
+    try {
+        const electron = require('electron');
+        if (typeof electron !== 'object') return false;
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
