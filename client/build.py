9416eac9cbe4c64e6f8d61c74f63d8f2a1209db9
Steve <b0rg3r@gmail.com>
2017-11-15 16:45:39 -0600

    Added bower install to client build script.

--- client/build.py
+++ client/build.py
@@ -3,11 +3,20 @@ import os
 cwd = os.path.dirname(os.path.realpath(__file__))
 
 
-def build_client(): # Build client assets.
-  path = 'lessc %s/static/styles/site.less %s/static/styles/site.css'
-  out = os.system(path % (cwd, cwd))  
-  print((path % (cwd, cwd)) + ' [' + str(out) + ']')
+def build_bower():  # Build bower dependencies from bower.json.
+    path = '%s\\static'
+    os.chdir(path % cwd)
+    path = 'bower install'
+    out = os.system(path)
+    print(path + ' [' + out + ']')
+
+
+def build_less():
+    path = 'lessc %s/static/styles/site.less %s/static/styles/site.css'
+    out = os.system(path % (cwd, cwd))
+    print((path % (cwd, cwd)) + ' [' + str(out) + ']')
 
 
 if __name__ == '__main__':
-  build_client()
+    build_bower()
+    build_less()
