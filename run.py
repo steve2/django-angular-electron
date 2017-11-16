df95552e1760672ed845f868c538e47a0fcdfd05
Steve <b0rg3r@gmail.com>
2017-11-16 17:16:35 -0600

    Updated build/setup scripts to be more PEP8 comformant.

--- run.py
+++ run.py
@@ -4,36 +4,51 @@ Runs the application server and client modules.
 
 from threading import Thread
 import argparse
-import sys
 import os
 
 DEBUG_PORT = 8080
 
-cwd = os.path.dirname(os.path.realpath(__file__))
-parser = argparse.ArgumentParser(description='Run the application.')
-parser.add_argument('--server', action='store_true')
-args = parser.parse_args()
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def _set_directory(subdir):
+    working_dir = os.path.join(ROOT_DIR, subdir)
+    os.chdir(working_dir)
+    print('Current directory: %s' % working_dir)
+
+
+def _run_command(command):
+    out = os.system(command)
+    print('%s [%d]' % (command, out))
+    if out is not 0:  # Raise an exception if the command fails.
+        raise Exception('Command failed: %s' % command)
 
 
 def _run_server():
-    path = '%s\\server\\env\\Scripts\\python.exe %s\\server\\manage.py runserver %s'
-    os.system(path % (cwd, cwd, DEBUG_PORT))
+    _set_directory('server')
+    python_path = os.path.join('env', 'Scripts', 'python.exe')
+    _run_command('%s manage.py runserver %s' % (python_path, DEBUG_PORT))
 
 
 def _run_electron():
-    path = '%s\\client\\node_modules\\electron-prebuilt\\dist\\electron.exe client'
-    os.system(path % cwd)
+    _set_directory('client')
+    electron_path = os.path.join(
+        'node_modules', 'electron-prebuilt', 'dist', 'electron.exe')
+    _run_command('%s .' % electron_path)
 
 
 def _build_client():
-    path = 'python %s\\client\\build.py'
-    os.system(path % cwd)
+    _set_directory('client')
+    _run_command('python build.py')
 
 
 def _main():
-    print('Building...')
+    parser = argparse.ArgumentParser(description='Run the application.')
+    parser.add_argument('--server', action='store_true')
+    args = parser.parse_args()
+
     _build_client()
-    print('Running...')
+
     server_thread = Thread(target=_run_server)
     server_thread.start()
     if not args.server:
--- setup.py
+++ setup.py
@@ -9,44 +9,87 @@ Runs the application server and client modules.
 """
 
 from threading import Thread
+from argparse import RawTextHelpFormatter
 import argparse
-import sys
 import os
 
 # The directory of this file should be the root directory of the project.
-rootDirectory = os.path.dirname(os.path.realpath(__file__))
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 # Path to virtualenv binaries for setup.
-pythonPath = os.path.join('env', 'Scripts', 'python.exe') 
-pipPath = os.path.join('env', 'Scripts', 'pip.exe')
+PYTHON = os.path.join('env', 'Scripts', 'python.exe')
+PIP = os.path.join('env', 'Scripts', 'pip.exe')
+
 
 def _set_directory(subdir):
-    workingDirectory = os.path.join(rootDirectory, subdir)
-    os.chdir(workingDirectory)
-    print('Current directory: %s' % workingDirectory)
+    working_dir = os.path.join(ROOT_DIR, subdir)
+    os.chdir(working_dir)
+    print('Current directory: %s' % working_dir)
 
 
 def _run_command(command):
     out = os.system(command)
     print('%s [%d]' % (command, out))
-    if out is not 0: # Raise an exception if the command fails.
+    if out is not 0:  # Raise an exception if the command fails.
         raise Exception('Command failed: %s' % command)
 
 
 def _main():
+    description = 'Setup development environment.\n\n' \
+        'If you have already run this script, it may be necessary to \n' \
+        'skip virtual environment and superuser setup. This can be \n' \
+        'done with options `--skip-venv` and `--skip-superuser`.'
+    parser = argparse.ArgumentParser(description=description,
+                                     formatter_class=RawTextHelpFormatter)
+    parser.add_argument('--skip-venv', action='store_true')
+    parser.add_argument('--skip-pip', action='store_true')
+    parser.add_argument('--skip-migrate', action='store_true')
+    parser.add_argument('--skip-superuser', action='store_true')
+    parser.add_argument('--skip-server', action='store_true')
+    parser.add_argument('--skip-client', action='store_true')
+    args = parser.parse_args()
+
     try:
         # Server setup.
-        _set_directory('server')
-        _run_command('virtualenv env')
-        _run_command('%s -m pip install --upgrade pip' % pythonPath)
-        _run_command('%s install -r requirements.txt' % pipPath)
-        _run_command('%s manage.py migrate' % pythonPath)
-        _run_command('%s manage.py createsuperuser' % pythonPath)
+        if not args.skip_server:
+            _set_directory('server')
+
+            # Initialize virtual environment.
+            if not args.skip_venv:
+                _run_command('virtualenv env')
+            else:
+                print('Skipping virtual environment setup.')
+
+            # Update pip to latest version.
+            if not args.skip_pip:
+                _run_command('%s -m pip install --upgrade pip' % PYTHON)
+            else:
+                print('Skipping pip update.')
+            _run_command('%s install -r requirements.txt' % PIP)
+
+            # Django migrations.
+            if not args.skip_migrate:
+                _run_command('%s manage.py migrate' % PYTHON)
+            else:
+                print('Skipping Django migrations.')
+
+            # Create Django superuser.
+            if not args.skip_superuser:
+                _run_command('%s manage.py createsuperuser' % PYTHON)
+            else:
+                print('Skipping Django Admin superuser creation.')
+        else:
+            print('Skipping server setup.')
+
         # Client setup.
-        _set_directory('client')
-        _run_command('npm install')
-        _set_directory(os.path.join('client', 'static'))
-        _run_command('bower install')
+        if not args.skip_client:
+            _set_directory('client')
+            _run_command('npm install')
+            _set_directory(os.path.join('client', 'static'))
+            _run_command('bower install')
+        else:
+            print('Skipping client setup.')
+
     except Exception as exception:
         print('Setup failed due to:\n\t%s' % str(exception))
 
