856f83191c54f292fa8ddff93196def5838d73d2
Steve <b0rg3r@gmail.com>
2017-11-16 23:29:25 -0600

    Tab indent level fix.

--- client/main.js
+++ client/main.js
@@ -4,14 +4,14 @@ var BrowserWindow = electron.BrowserWindow;
 var window = null;
 
 app.on('window-all-closed', function() {
-    if (process.platform != 'darwin')
-        app.quit();
+  if (process.platform != 'darwin')
+    app.quit();
 });
 
 app.on('ready', function() { 
-    window = new BrowserWindow({width: 800, height: 600}); 
-    window.loadURL('file://' + __dirname + '/index.html'); 
-    window.on('closed', function() {
-        window = null;
-    }); 
+  window = new BrowserWindow({width: 800, height: 600}); 
+  window.loadURL('file://' + __dirname + '/index.html'); 
+  window.on('closed', function() {
+    window = null;
+  }); 
 });
