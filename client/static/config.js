b550f49c3755eb959023feae1d6aff5f38a345ed
Steve <b0rg3r@gmail.com>
2017-11-17 18:12:58 -0600

    Added some authentication/authorization enforcement to AngularJS application.

--- client/static/config.js
+++ client/static/config.js
@@ -11,9 +11,8 @@ angular
   .run(Run);
 
 Config.$inject = ["$httpProvider", "$resourceProvider", "$locationProvider"];
-Run.$inject = ["$rootScope", "$state", "$location", "$transitions", "$q"];
+Run.$inject = ["$rootScope", "$state", "$location", "$transitions", "$q", "Session"];
 
-// Application configuration.
 function Config ($httpProvider, $resourceProvider, $locationProvider) {
   $httpProvider.defaults.xsrfCookieName = "csrftoken";
   $httpProvider.defaults.xsrfHeaderName = "X-CSRFToken";
@@ -21,17 +20,45 @@ function Config ($httpProvider, $resourceProvider, $locationProvider) {
   $locationProvider.html5Mode({ enabled: true, requireBase: false });
 }
 
-// Application initialization.
-function Run ($rootScope, $state, $location, $transitions, $q) {
-  
-  // Template utility functions.
+function Run ($rootScope, $state, $location, $transitions, $q, Session) {  
+
+  // These functions can be used by any AngularJS templates.
   $rootScope.moment = moment;
   $rootScope.link = $state.go;
   $rootScope.isElectron = isElectron; // Returns true if Electron is running.
   $rootScope.formatDate = function (str) { return moment(str).format('LL'); };
   $rootScope.formatDateTime = function (str) { return moment(str).format('LLL'); };
 
+  // Initialize application session.
+  $rootScope.user = Session.current();
+  $rootScope.user.$promise.then(
+    function (data) {
+      if ($state.current.registration)
+        $state.go("root.profile");
+      WatchTransitions();
+    },
+    function (errors) {
+      $rootScope.user = null;
+      let targetState = $state.get().filter(state => (state.url == $location.url()))[0];
+      if (targetState === undefined || targetState.protected)
+        $state.go("root.login");
+      WatchTransitions();
+    }
+  );
 
+  // Control access to specific states.
+  function WatchTransitions() {
+    $transitions.onBefore({ to: "**" }, function ($state) {
+      if ($state.to().protected && !$rootScope.user) {
+        $state.router.stateService.target("root.login");
+        return false;
+      }
+      else if ($rootScope.user && $state.to().registration) {
+        $state.router.stateService.target("root.home");
+        return false;
+      }
+    });
+  }
 
 }
 
--- client/static/router.js
+++ client/static/router.js
@@ -12,6 +12,17 @@ function Router ($stateProvider, $urlRouterProvider, $locationProvider) {
   // Default state.
   $urlRouterProvider.otherwise("/");
 
+  // There are currently three types of states.
+  //  1) States that logged in users shouldn't see (Login, Signup, etc.).
+  //  2) States that only logged in users should see (Profile, etc.).
+  //  3) States that are neither of the above.
+  //
+  // These are categorized by additional properties added to state objects.
+  //  "registration": Corresponds with situation (1) above.
+  //  "protected": Corresponds with situation (2) above.
+  //  <no property>: Corresponds with situation (3) above.
+  // 
+
   // State definitions.
   $stateProvider
     .state("root", {
@@ -25,20 +36,23 @@ function Router ($stateProvider, $urlRouterProvider, $locationProvider) {
       templateUrl: "static/states/Home/Home.html",
       controller: "Home as Home"
     })
+    .state("root.signup", {
+      url: "signup/",
+      templateUrl: "static/states/Signup/Signup.html",
+      controller: "Signup as Signup",
+      registration: true
+    })
     .state("root.login", {
       url: "login/",
       templateUrl: "static/states/Login/Login.html",
-      controller: "Login as Login"
+      controller: "Login as Login",
+      registration: true
     })
     .state("root.profile", {
       url: "profile/",
       templateUrl: "static/states/Profile/Profile.html",
-      controller: "Profile as Profile"
-    })
-    .state("root.signup", {
-      url: "signup/",
-      templateUrl: "static/states/Signup/Signup.html",
-      controller: "Signup as Signup"
+      controller: "Profile as Profile",
+      protected: true
     });
 }
 
