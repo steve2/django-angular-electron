6d59f60e9f7ec3a3c44fb8e336fd53f9e623f348
Steve <b0rg3r@gmail.com>
2017-11-18 01:20:43 -0600

    Added "Logout" control to dashboard, some styling.

--- client/static/states/dashboard/dashboard.js
+++ client/static/states/dashboard/dashboard.js
@@ -5,13 +5,18 @@ angular
   .module("app")
   .controller("DashboardController", DashboardController);
 
-DashboardController.$inject = ["$timeout", "$element"];
+DashboardController.$inject = ["$timeout", "$element", "$state", "$stateParams", "Session"];
 
-function DashboardController ($timeout, $element) {
+function DashboardController ($timeout, $element, $state, $stateParams, Session) {
   var ct = this;
 
-  
-
+  ct.logout = function () {
+    Session.logout().$promise.then(
+      function (data) {
+        $state.transitionTo($state.current, $stateParams, { reload: true });
+      }
+    );
+  };
 }
 
 })();
