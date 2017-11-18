b6d41dcdb22750e1e80485660a4cbde56ff1a99c
Steve <b0rg3r@gmail.com>
2017-11-18 00:23:34 -0600

    Renamed "Home" state to "Dashboard".

---
+++ client/static/states/dashboard/dashboard.js
@@ -0,0 +1,17 @@
+(function () {
+"use strict";
+
+angular
+  .module("app")
+  .controller("DashboardController", DashboardController);
+
+DashboardController.$inject = ["$timeout", "$element"];
+
+function DashboardController ($timeout, $element) {
+  var ct = this;
+
+  
+
+}
+
+})();
