ca1f7be348c37d10aed482b23e9f3bf86116d4e0
Steve <b0rg3r@gmail.com>
2017-11-18 01:19:59 -0600

    Added Login input.

--- client/static/states/Login/Login.js
+++ client/static/states/Login/Login.js
@@ -5,12 +5,18 @@ angular
   .module("app")
   .controller("LoginController", LoginController);
 
-  LoginController.$inject = ["$timeout", "$element"];
+  LoginController.$inject = ["$timeout", "$element", "$state", "$stateParams", "Session"];
 
-function LoginController ($timeout, $element) {
+function LoginController ($timeout, $element, $state, $stateParams, Session) {
   var ct = this;
 
-  
+  ct.login = function (username, password) {
+    Session.login(username, password).$promise.then(
+      function (data) {
+        $state.transitionTo($state.current, $stateParams, { reload: true });
+      }
+    )
+  };
 
 }
 
