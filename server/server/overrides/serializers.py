33369665248afc7f6f94ba9b44e5da87d5ed3a43
Steve <b0rg3r@gmail.com>
2017-11-19 00:57:11 -0600

    ProfileSerializer is read_only for now until an update method is developed.

--- server/server/overrides/serializers.py
+++ server/server/overrides/serializers.py
@@ -3,14 +3,14 @@ from rest_framework import serializers
 from profiles.serializers import ProfileSerializer
 
 # Get Django UserModel.
-UserModel = get_user_model()
+USER_MODEL = get_user_model()
 
 class UserDetailsSerializer(serializers.ModelSerializer):
     """
     User model with profile.
     """
-    profile = ProfileSerializer()
+    profile = ProfileSerializer(read_only=True)
     class Meta:
-        model = UserModel
+        model = USER_MODEL
         fields = ('pk', 'username', 'email', 'first_name', 'last_name', 'profile')
         read_only_fields = ('email',)
