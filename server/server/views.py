150af2fe2e2602e944ac0cf372f6a64f168880b1
Steve <b0rg3r@gmail.com>
2017-11-19 19:26:24 -0600

    Deleted unnecessary view.

--- server/server/views.py
+++ server/server/views.py
@@ -1,6 +1,8 @@
 # from django.shortcuts import render
 from django.views.generic import TemplateView
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
+from allauth.socialaccount.providers.twitch.views import TwitchOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from rest_auth.registration.views import SocialLoginView
 
 
@@ -9,10 +11,3 @@ class IndexTemplate(TemplateView):
     The base template for our AngularJS application client.
     """
     template_name = 'index.html'
-
-
-class FacebookLogin(SocialLoginView):
-    """
-    Facebook social auth view.
-    """
-    adapter_class = FacebookOAuth2Adapter
