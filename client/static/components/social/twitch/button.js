e0f47d6e42ed34b26f0731fafb712e18a839a859
Steve <b0rg3r@gmail.com>
2017-11-19 19:25:23 -0600

    Added Twitch button component.

---
+++ client/static/components/social/twitch/button.js
@@ -0,0 +1,27 @@
+(function () {
+"use strict";
+  
+angular
+  .module("app")
+  .directive("twitchButton", TwitchButton);
+
+TwitchButton.$inject = [];
+
+function TwitchButton () {
+  TwitchButtonController.$inject = ['$scope'];
+
+  function TwitchButtonController ($scope) {
+    var ct = $scope;
+    
+
+  }
+
+  return {
+    restrict: 'E',
+    scope: { click: '=' },
+    controller: TwitchButtonController,
+    templateUrl: "/static/components/social/twitch/button.html"
+  };
+}
+  
+  })();
