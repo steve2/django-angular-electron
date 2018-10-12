c648d2b6ed46d1180484bd36d4bd2b64b4cd8af4
Steve <b0rg3r@gmail.com>
2018-10-12 18:43:15 -0500

    Added some comments to `build.py`.

--- client/build.py
+++ client/build.py
@@ -1,15 +1,23 @@
+"""
+Builds the client project. This currently involves:
+    - Compiling LESS styles into single CSS file.
+"""
 import os
 
-clientDirectory = os.path.dirname(os.path.realpath(__file__))
+
+# The root directory of the client (directory of this file).
+CLIENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def _set_directory(subdir):
-    workingDirectory = os.path.join(clientDirectory, subdir)
-    os.chdir(workingDirectory)
-    print('Current directory: %s' % workingDirectory)
+    """Set the current working directory"""
+    working_dir = os.path.join(CLIENT_DIR, subdir)
+    os.chdir(working_dir)
+    print('Current directory: %s' % working_dir)
 
 
 def _run_command(command):
+    """Run a system command and print the output"""
     out = os.system(command)
     print('%s [%d]' % (command, out))
     if out is not 0: # Raise an exception if the command fails.
@@ -17,7 +25,8 @@ def _run_command(command):
 
 
 def compile_less():
-    _set_directory(os.path.join(clientDirectory, 'static', 'styles'))
+    """Compile LESS styles into single CSS file"""
+    _set_directory(os.path.join(CLIENT_DIR, 'static', 'styles'))
     _run_command('lessc site.less site.css')
 
 
